9ab986587e4809c167950238396881bb880c633b
Jinyan Liu <jinyan.liu@bontouch.com>
2021-03-15 10:26:42 +0100

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Jinyan Liu on 2021-03-15.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
